4948ae9bf5da147a1ae620446df878e983cddb33
waixiong <waixiong@siswa.um.edu.my>
2019-02-15 05:15:06 +0800

    Updates

---
+++ precache-manifest.5cc94cbea599ee4f7245f9924f008714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyes-population/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2468e9f0460978ab8001",
+    "url": "/nyes-population/static/js/runtime~main.2468e9f0.js"
+  },
+  {
+    "revision": "aa71043fe79bd16f065f",
+    "url": "/nyes-population/static/js/main.aa71043f.chunk.js"
+  },
+  {
+    "revision": "16ebeb9211417aa6dda4",
+    "url": "/nyes-population/static/js/1.16ebeb92.chunk.js"
+  },
+  {
+    "revision": "aa71043fe79bd16f065f",
+    "url": "/nyes-population/static/css/main.9a6a312a.chunk.css"
+  },
+  {
+    "revision": "16ebeb9211417aa6dda4",
+    "url": "/nyes-population/static/css/1.6b46d28d.chunk.css"
+  },
+  {
+    "revision": "414d7c3c68b61a379910ae47b7a3564f",
+    "url": "/nyes-population/index.html"
+  }
+];
